8ca1eca5c38f61d6d0e110c75a836923e3d3e574
ChestnutLUO <digby@65mail.cn>
2026-02-10 07:35:37 +0800

    fix: resolve nav overlap by removing justify-center and adjusting padding

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -112,7 +112,7 @@ const HeroSection: React.FC = () => {
   return (
     <section
       ref={containerRef}
-      className="relative min-h-screen w-full flex flex-col justify-center px-4 md:px-12 pt-32 overflow-hidden"
+      className="relative min-h-screen w-full flex flex-col px-4 md:px-12 pt-36 md:pt-44 overflow-hidden"
     >
       {/* Background Grid */}
       <div
